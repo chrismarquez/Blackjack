84ed8b8a0d94445bcefc888b24caa15164823ada
chrismarquez <a01225198@itesm.mx>
2015-04-22 23:33:48 -0500

    1st Commit
    Recovers all changes
    Deck can now be shuffled

---
+++ Blackjack Project/src/GameControl.java
@@ -0,0 +1,21 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+
+public class GameControl extends JPanel implements ActionListener{
+
+	public GameControl() {
+		super();
+		this.setPreferredSize(new Dimension(800, 100));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
